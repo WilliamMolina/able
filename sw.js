ecbbdbd377545c68d4323d2b21434f4c92a26d74
William Molina <wdmolina@bancolombia.com.co>
2022-03-17 19:56:24 -0500

    actualizar service worker

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ workbox.setConfig({
   debug: true,
 });
 
-console.log(workbox)
 self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('main').then(function(cache) {
@@ -26,16 +25,7 @@ self.addEventListener('install', function(event) {
     );
   });
 
-  /* Cache only
-  self.addEventListener('fetch', function(event) {
-    console.log(event.request);
-    event.respondWith(caches.match(event.request));
-  });
-  */
-
   function cacheOnly(event){
-    console.log('Cache first');
-    console.log(event.request.url);
     event.respondWith(caches.match(event.request));
   }
 
@@ -46,14 +36,11 @@ self.addEventListener('install', function(event) {
           if(event.request.url.includes('ngrok')){
             let cached = '/'+event.request.url.split('/').slice(3).join('/');
             let r = response.clone();
-            console.log(cached);
             localforage.getItem(cached, function(err, value){
-              console.log(cached+' '+value);
               if(!value){
                 cache.put(cached, r);
                 localforage.setItem(cached,true);
-              }else
-                console.log("Actualizado")
+              }
             });
           }else{
             cache.put(event.request, response.clone());
@@ -74,7 +61,6 @@ self.addEventListener('install', function(event) {
   });
 
   const handlerCb = ({url, event, params}) => {
-    console.log(url);
     return fetch('/demo-popper.png');
   };
 
